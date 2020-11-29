2070309d0febbab48e5f9e4bfd11922ddf89b80e
rizki4bdil4h <rizki4bdil4h@gmail.com>
2020-11-29 15:59:08 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>hello word</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptates, modi.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 
 </html>
